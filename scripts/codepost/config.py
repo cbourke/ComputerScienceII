9417d3e4a7436f7beefe549de816044b44af3bfb
Chris Bourke <cbourke@cse.unl.edu>
2019-12-22 18:25:25 -0600

    updated config file to include sql files and to have a relative path for the handin dir

--- scripts/codepost/config.py
+++ scripts/codepost/config.py
@@ -9,17 +9,21 @@ class Config:
     
     # Canvas API configuration
     canvasUrl      = "https://canvas.unl.edu/api/v1/"
-    canvasApiKey   = "" # 
-    canvasCourseId = "" # 
+    canvasApiKey   = ""
+    canvasCourseId = "" 
     
     # codepost.io configuration
     codePostApiKey = ""
-    codePostCourseId = -1 # 
+    codePostCourseId = None 
     
     # file extensions/types to include in codepost push
     # leave empty to include all files
-    fileExtensions = [".java", ".php", ".jar"]
+    fileExtensions = [".java", ".php", ".jar", ".sql"]
         
+    # Relative CSE Handin Path
+    # By default, these scripts are assumed to be in ~/codepost
+    handinPath = "../handin/"
+    
     # CSE User Database configuration (udb on cse-apps)
     # cse-apps.unl.edu is firewalled for 3306 except from cse/csce
     nuidToCseLoginPickle = "nuidToCseLogin.pkl"
@@ -35,11 +39,9 @@ class Config:
     #  Graders are given "super grader" access to codepost and assigned to 
     #   grade.  An instructor must also be a grader if you want them to be
     #   assigned grading duties.
-    instructorNuids = ["35140602", #Bourke
-                       
+    instructorNuids = ["35140602", # Bourke
                        ]
-    graderNuids = ["35140602", #Bourke
-
+    graderNuids = ["35140602", # Bourke
                  ]
 
 config = Config()
