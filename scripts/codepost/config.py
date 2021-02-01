0b219942a416f92ed1eb8a96ab0c6c6adddd5ea2
Chris Bourke <>
2021-02-01 11:39:06 -0600

    added group support

--- scripts/codepost/config.py
+++ scripts/codepost/config.py
@@ -1,10 +1,19 @@
-# Changes will not be tracked on this file to avoid committing
-# keys and passwords, if other changes are made, it will need
-# to be restored to the index
-#   git update-index --no-assume-unchanged config.py
-# and then removed again:
-#   git update-index --assume-unchanged config.py
-# 
+"""
+Master configuration file for all scripts.  
+
+This configuration file will contain sensitive information
+(NUIDs, passwords) and should *not* be tracked or committed
+to a remote repository (even a private one).  If changes
+do need to be made, sensitive information should be removed
+before hand.  
+
+To help avoid committing sensitive information, you should
+set this file to be untracked: 
+   git update-index --assume-unchanged config.py
+To track the file again you can use:
+   git update-index --no-assume-unchanged config.py
+
+"""
 class Config:
     
     # Canvas API configuration
@@ -25,7 +34,11 @@ class Config:
     handinPath = "/home/grad/Classes/cse156/handin/"
     
     # CSE User Database configuration (udb on cse-apps)
-    # cse-apps.unl.edu is firewalled for 3306 except from cse/csce
+    # cse-apps.unl.edu is firewalled for 3306 except from 
+    # cse/csce
+    #  - The pickle file is used to locally store database information
+    #    so that it doesn't have to requery for every script execution 
+    #  - Credentials must be obtained from CSE System Administration
     nuidToCseLoginPickle = "nuidToCseLogin.pkl"
     class udb:
         driver   = "libmaodbc"
@@ -34,6 +47,10 @@ class Config:
         username = ""
         password = ""
 
+    # To use canvas groups for assigning grading, set the name 
+    # of the group here.  
+    gradingGroupName = "Project Pairs"
+
     # Course-specific role configurations
     #  Instructors are given codepost admin access, but not assigned to grade
     #  Graders are given "super grader" access to codepost and assigned to 
@@ -41,6 +58,7 @@ class Config:
     #   assigned grading duties.
     instructorNuids = ["35140602", # Bourke
                       ]
+    # All graders' NUIDs
     graderNuids = ["35140602", # Bourke
                   ]
 
