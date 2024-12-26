939832b4a2c262ec73782e4e0ea0cff260e0819a
Chris Bourke <cbourke@cse.unl.edu>
2024-12-25 19:49:41 -0600

    modified attendance script

--- scripts/codepost/update_attendance.py
+++ scripts/codepost/update_attendance.py
@@ -10,8 +10,14 @@ This script interfaces with the UNL Canvas instance to update the attendance
 points according to this policy.
 """
 
-from config import config
 import argparse
+from types import SimpleNamespace
+import sys
+
+from course import course
+from canvasUtils import getAssignments
+from canvasUtils import getGrade
+from canvasUtils import setGrade
 
 parser = argparse.ArgumentParser()
 parser.add_argument("module_name", help=
@@ -25,87 +31,54 @@ parser.add_argument("--commit", action='store_true', help=
   """)
 args = parser.parse_args()
 
-from course import course
-from assignment import Assignment
-from canvasUtils import getAssignments
-from canvasUtils import getGrade
-from canvasUtils import setGrade
-import requests
-import pprint
-from types import SimpleNamespace
+module_name = args.module_name
+commit_to_canvas = args.commit
 
-module_name = args.moduleName
-commitToCanvas = args.commit
-
-def get_module(module_name):
-  """
-  Returns a "module" representation of a CS2 module in canvas (lab and
-  attendance) for the module with the given `module_name`
-  """
-
-  assignments = getAssignments(module_name)
-
-  result = SimpleNamespace()
-  result.lab = next(filter(lambda x: 'Lab' in x.name and 'Attendance' not in x.name, assignments), None)
-  result.attendance = next(filter(lambda x: 'Module' in x.name and 'Reading' not in x.name, assignments), None)
-
-  return result
-
-def updateAttendance(module, student):
+def update_attendance(module, student):
     """
     Updates the "differential attendance" score in the canvas gradebook for the
     given module and student.
 
-    * If the attendance score for both a lab and hack are already awarded,
-      no action is taken
-    * If the score(s) necessary to determine if attendance should be awarded,
-      a warning is printed and no action is taken
-    * If the scores on the reading/lab/hack meet the threshold (>=75%), the
-      attendance scores are updated along with a comment
-    * Otherwise, the scores are not modified, but a comment is added
+    * If the attendance score for attendance is already set no action is taken
+    * If the lab score is 20, attendance points are awarded
+    * If the lab score is less than 20, no attenacne points are awarded
     """
-    print(f"Processing student: {student}...")
-    readingScore = getGrade(module.reading.id, student.canvasId)
-    labScore = getGrade(module.lab.id, student.canvasId)
-    hackScore = getGrade(module.hack.id, student.canvasId)
-    attendScore = getGrade(module.attendance.id, student.canvasId)
+    print(f'Processing student: {student}...')
 
-    r = [readingScore, labScore, hackScore, attendScore]
-    if None in r:
-        print(f"\tNot updating attendance due to missing score")
-        return
 
-    comment = f"Original attendance score: {attendScore}/10.  "
+    lab_score = getGrade(module.lab.id, student.canvas_id)
+    attend_score = getGrade(module.attendance.id, student.canvas_id)
 
-    if attendScore < 10:
-        subTotal = readingScore + labScore + hackScore
-        percent = subTotal / 55.0
-        if percent >= .75:
-            comment += f"75% threshold met ({subTotal:.2f} / 55.0 = {percent*100:.2f}%).  Attenance points awarded."
-            attendScore = 10
+    comment = f'Original attendance score: {attend_score}/5.'
+
+    if attend_score < 5:
+        if lab_score >= 20:
+            attend_score = 5
+            comment += f'Lab completed, attendance points awarded: {attend_score}/5'
         else:
-            comment += f"75% threshold NOT met ({subTotal:.2f} / 55.0 = {percent*100:.2f}%)."
+            comment += f'Lab not complete, attendance points not awarded: {attend_score}/5'
 
     print("\t",comment)
 
-    if commitToCanvas:
-        setGrade(module.attendance.id, student.canvasId, attendScore, comment)
+    if commit_to_canvas:
+        setGrade(module.attendance.id, student.canvas_id, attend_score, comment)
 
-module = getModule(moduleName)
-print(f"Processing Attendance for {moduleName}...")
+print(f"Processing Attendance for {module_name}...")
+assignments = getAssignments(module_name)
+module = SimpleNamespace()
+module.lab        = next(filter(lambda x: 'Lab' in x.name and x.points == 20, assignments), None)
+module.attendance = next(filter(lambda x: 'Lab' in x.name and x.points == 5, assignments), None)
 print("Assignments:")
-print(f"  Reading: {module.reading}")
 print(f"  Lab:     {module.lab}")
-print(f"  Hack:    {module.hack}")
 print(f"  Attend:  {module.attendance}")
 
 a = input("Proceed? Y/N")
 
 if a != "Y":
     print("Cowardly exiting...")
-    exit(1)
+    sys.exit(1)
 
 for (nuid,s) in course.students.items():
-    updateAttendance(module, s)
-if not commitToCanvas:
+    update_attendance(module, s)
+if not commit_to_canvas:
     print("Cowardly refused to commit changes to canvas, run with --commit jabroni.")
