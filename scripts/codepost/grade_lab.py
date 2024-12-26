123b02e13325f163a7ca7541ec5e3b3a788228e4
Chris Bourke <cbourke@cse.unl.edu>
2024-12-25 20:43:54 -0600

    cleaned whitespace PEP 8

--- scripts/codepost/grade_lab.py
+++ scripts/codepost/grade_lab.py
@@ -26,16 +26,26 @@ junit_tests = {
     11: "unl.soc.trucks.TruckListTests",
     13: "unl.soc.sorting.SortingTests",
     14: "unl.soc.JsonValidatorTests unl.soc.PostfixEvaluatorTests",
-    15: "unl.soc.BinarySearchTreeTests unl.soc.HeapSortTests"
+    15: "unl.soc.BinarySearchTreeTests unl.soc.HeapSortTests",
 }
 
 parser = argparse.ArgumentParser()
-parser.add_argument("lab_number", help="The lab number to be graded (ex: 1 would be Lab 1.0 in in ~/handin/L01)", type=int)
-parser.add_argument("--login", help="specify to grade a single canvas login", default=None)
-parser.add_argument("--commit", action='store_true', help=
-  """Push the lab scores to canvas. The default is to not
+parser.add_argument(
+    "lab_number",
+    help="The lab number to be graded (ex: 1 would be Lab 1.0 in in ~/handin/L01)",
+    type=int,
+)
+parser.add_argument(
+    "--login", help="specify to grade a single canvas login", default=None
+)
+parser.add_argument(
+    "--commit",
+    action="store_true",
+    help="""Push the lab scores to canvas. The default is to not
   modify the canvas gradebook so a test run can be made.
-  """, default=False)
+  """,
+    default=False,
+)
 args = parser.parse_args()
 push_to_canvas = args.commit
 if push_to_canvas:
@@ -63,7 +73,7 @@ login = args.login
 
 # If login is specified, we cut the roster to a single student
 if login:
-    students = {k:v for (k,v) in course.students.items() if v.canvas_login == login}
+    students = {k: v for (k, v) in course.students.items() if v.canvas_login == login}
 else:
     students = course.students
 
@@ -84,58 +94,58 @@ javac = "/usr/bin/javac"
 print(f"Grading {lab_name} using {junit_test_classes} in {lab_number}...")
 print(f"  Canvas Assignment: {canvas_assignment}")
 
-(good,bad,ugly) = (0,0,0)
+(good, bad, ugly) = (0, 0, 0)
 
-for nuid,p in students.items():
-  print(f"  {p.name} ({p.nuid})...")
-  login = p.canvas_login
-  canvas_id = p.canvas_id
-  full_path = f"{base_path}{lab_number}/{login}/"
-  score = getGrade(canvas_assignment.id, p.canvas_id)
-  if score is not None:
-    print(f"    SKIPPED: Score already in gradebook: {score}/20")
-  elif not os.path.exists(full_path):
-    print(f"    FAILED: no directory ({full_path}), 0/20")
-    comment = f"No Submission 0/20"
-    if push_to_canvas:
-        setGrade(canvas_assignment.id, p.canvas_id, 0, comment)
-    print(f"\tRESULT: {comment}")
-    ugly += 1
-  else:
-    os.chdir(base_path + lab_number + "/" + login)
-    os.system("rm -rf "+staging_dir)
-    os.mkdir(staging_dir)
-    os.system(f"cp ../../common/{test_wrapper_file} ./{staging_dir}")
-    os.system(f"cp ../../common/{junit_jar} ./{staging_dir}")
-    #copy any course-level jars that may be needed
-    os.system(f"cp -R ../../common/lib ./{staging_dir}");
-    os.system(f"cp -R ../common/* ./{staging_dir}")
-    os.system(f"cp *.java ./{staging_dir}")
-    os.chdir(staging_dir)
-
-    jars = ":".join(glob.glob("./lib/*.jar"))
-    javac_cmd = f"{javac} -cp ./:./{junit_jar}:{jars} -d . *.java"
-
-    os.system(javac_cmd)
-
-    java_cmd = f"{java} -cp ./:./{junit_jar}:{jars} {test_wrapper_class} {junit_test_classes}"
-
-    result = subprocess.run([java_cmd, ""], shell=True, stdout=subprocess.PIPE)
-    output = result.stdout.decode('utf-8')
-    num_fail = result.returncode
-    if not result or num_fail > 0:
-        comment = f"Did not compile or run or one or more JUnit tests failed: 0/20"
-        score = 0
-        bad += 1
+for nuid, p in students.items():
+    print(f"  {p.name} ({p.nuid})...")
+    login = p.canvas_login
+    canvas_id = p.canvas_id
+    full_path = f"{base_path}{lab_number}/{login}/"
+    score = getGrade(canvas_assignment.id, p.canvas_id)
+    if score is not None:
+        print(f"    SKIPPED: Score already in gradebook: {score}/20")
+    elif not os.path.exists(full_path):
+        print(f"    FAILED: no directory ({full_path}), 0/20")
+        comment = f"No Submission 0/20"
+        if push_to_canvas:
+            setGrade(canvas_assignment.id, p.canvas_id, 0, comment)
+        print(f"\tRESULT: {comment}")
+        ugly += 1
     else:
-        comment = f"All JUnit tests passed: {points}/{points}"
-        score = points
-        good += 1
-    if push_to_canvas:
-        setGrade(canvas_assignment.id, p.canvas_id, score, comment)
-    print(f"\tRESULT: {comment}, {score}")
-    os.chdir("..")
-    os.system("rm -rf "+staging_dir)
+        os.chdir(base_path + lab_number + "/" + login)
+        os.system("rm -rf " + staging_dir)
+        os.mkdir(staging_dir)
+        os.system(f"cp ../../common/{test_wrapper_file} ./{staging_dir}")
+        os.system(f"cp ../../common/{junit_jar} ./{staging_dir}")
+        # copy any course-level jars that may be needed
+        os.system(f"cp -R ../../common/lib ./{staging_dir}")
+        os.system(f"cp -R ../common/* ./{staging_dir}")
+        os.system(f"cp *.java ./{staging_dir}")
+        os.chdir(staging_dir)
+
+        jars = ":".join(glob.glob("./lib/*.jar"))
+        javac_cmd = f"{javac} -cp ./:./{junit_jar}:{jars} -d . *.java"
+
+        os.system(javac_cmd)
+
+        java_cmd = f"{java} -cp ./:./{junit_jar}:{jars} {test_wrapper_class} {junit_test_classes}"
+
+        result = subprocess.run([java_cmd, ""], shell=True, stdout=subprocess.PIPE)
+        output = result.stdout.decode("utf-8")
+        num_fail = result.returncode
+        if not result or num_fail > 0:
+            comment = f"Did not compile or run or one or more JUnit tests failed: 0/20"
+            score = 0
+            bad += 1
+        else:
+            comment = f"All JUnit tests passed: {points}/{points}"
+            score = points
+            good += 1
+        if push_to_canvas:
+            setGrade(canvas_assignment.id, p.canvas_id, score, comment)
+        print(f"\tRESULT: {comment}, {score}")
+        os.chdir("..")
+        os.system("rm -rf " + staging_dir)
 
 print(f"Passed: {good}")
 print(f"Failed: {bad} ")
