3886989f69f3d577f2c18540ad7796be2682fcbe
Chris Bourke <cbourke@cse.unl.edu>
2025-02-04 16:12:43 -0600

    added starter file for exercise 2

---
+++ assignments/1251-Spring2025/assignment00-files/RunningUtils.java
@@ -0,0 +1,25 @@
+package unl.soc;
+
+import java.util.List;
+
+/**
+ * TODO: class and author documentation
+ *
+ */
+public class RunningUtils {
+
+	/**
+	 * TODO: documentation for this method
+	 */
+	public static double elevationGain(List<Double> elevations) {
+		//TODO: implement
+	}
+
+	/**
+	 * TODO: documentation for this method
+	 */
+	public static double[][] convertMap(double elevations[][]) {
+		//TODO: implement
+	}
+
+}
